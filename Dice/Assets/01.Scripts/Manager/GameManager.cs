068a82f21460bdca32f02926e7e643c50c0b1399
Jongyup Kim <0327max@gmail.com>
2025-01-10 17:45:17 +0900

    매니저 추가

---
+++ Dice/Assets/01.Scripts/Manager/GameManager.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+public class GameManager : MonoBehaviour
+{
+
+
+    #region Singleton Init
+    public static GameManager Instance { get; private set; }
+    private void Awake()
+    {
+        // 싱글톤 패턴 구현
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject); // 기존 인스턴스가 있을 경우 새 인스턴스를 삭제
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject); // 씬 전환 시 파괴되지 않음
+    }
+    #endregion
+
+    #region Util
+
+    public void InitializeGame()
+    {
+        //초기화 로직
+    }
+
+    public void EndGame()
+    {
+        //강제 종료 로직
+    }
+
+    #endregion
+}
