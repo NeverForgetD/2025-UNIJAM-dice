d8513e4dc3efd20b138f0a1525aee0f4e2372a43
Jongyup Kim <0327max@gmail.com>
2025-01-10 22:26:50 +0900

    poolable

---
+++ Dice/Assets/01.Scripts/Upgrade/UpgradeController.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpgradeController : MonoBehaviour
+{
+    #region Upgrade Type
+    private enum UpgradeType
+    {
+        Dice,
+        Bonus,
+    }
+
+    [SerializeField] private UpgradeType upgradeType;
+    #endregion
+
+
+    #region privates
+
+    #endregion
+
+    #region Unity LifeCycle
+    private void OnEnable()
+    {
+
+    }
+
+    private void OnDisable()
+    {
+
+    }
+
+    #region SerializedField
+    [SerializeField] private Image optionImage1;
+    [SerializeField] private Image optionImage2;
+    [SerializeField] private Image optionImage3;
+    #endregion
+
+
+
+
+    #endregion
+
+
+
+}
