b35007901e310807742c16d8a57b207a743234d1
Jongyup Kim <0327max@gmail.com>
2025-01-11 20:28:23 +0900

    교체 메서드 적용

--- Dice/Assets/01.Scripts/Battle/Status.cs
+++ Dice/Assets/01.Scripts/Battle/Status.cs
@@ -24,7 +24,7 @@ public class Status// : MonoBehaviour
     }
 
     /// <summary>
-    /// 플레이어 스탯값에 값을 더하거나 뺍니다. {hp, atk, def, pot} {정수값 + -}
+    /// 플레이어 스탯값을 변경 {hp, atk, def, pot} {정수값 + -} / hp 는 추가
     /// </summary>
     /// <param name="stat"></param>
     /// <param name="value"></param>
@@ -35,17 +35,17 @@ public class Status// : MonoBehaviour
             case ("hp"):
                 hp += value; break;
             case ("atk"):
-                atk += value; break;
+                atk = value; break;
             case ("def"):
-                def += value; break;
+                def = value; break;
             case ("pot"):
-                pot += value; break;
+                pot = value; break;
             default:
                 Debug.Log($"{stat} wrong name : hp, atk, def, pot");
                 break;
         }
     }
-
+    /*
     public void ChangeStatus(string stat, int value)
     {
         switch (stat)
@@ -63,7 +63,7 @@ public class Status// : MonoBehaviour
                 break;
         }
     }
-
+    */
     public void PrintStatus(string stat)
     {
         switch (stat)
